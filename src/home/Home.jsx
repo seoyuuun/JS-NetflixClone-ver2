3651a08826c9e54bd227332f7aa6854f5fd485fb
seoyuun <carpedm1221@gmail.com>
2022-01-13 15:56:55 +0900

    navbar 추가 완료, main topbanner 시작

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,6 +5,113 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <img
+        width="100%"
+        src="https://i.pinimg.com/originals/c2/0b/c0/c20bc075564c08b38ac3c43e12f39eff.jpg"
+        alt=""
+      />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      repellat odio? Maxime maiores nulla at rerum id voluptatum officiis magni
+      a doloribus ducimus qui omnis, nihil delectus dolores soluta possimus!
     </div>
   );
 };
