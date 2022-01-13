43cf529921c201a61ee482ed28ceba67e3b92e47
seoyuun <carpedm1221@gmail.com>
2022-01-13 23:32:13 +0900

    slider list 레이아웃 설정

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import "./list.scss";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import ListItem from "./ListItem";
 
 const List = () => {
+  const hadleClick = (direction) => {};
+
   return (
     <div className="list">
       <span className="listTitle">Continue to Watch</span>
       <div className="wrapper">
-        <IoIosArrowBack />
-        <div className="container"></div>
-        <IoIosArrowForward />
+        <IoIosArrowBack
+          className="slider_arrow left"
+          onClick={() => hadleClick(left)}
+        />
+        <div className="container">
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </div>
+        <IoIosArrowForward
+          className="slider_arrow right"
+          onClick={() => hadleClick(right)}
+        />
       </div>
     </div>
   );
--- src/components/list/ListItem.jsx
+++ src/components/list/ListItem.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./listItem.scss";
 
 const ListItem = () => {
-  return <div></div>;
+  return <div className="listItem">item</div>;
 };
 
 export default ListItem;
