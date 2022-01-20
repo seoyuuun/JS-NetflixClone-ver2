930c8e83bb3d48db535b2ee970a91d3f7d7d8fc3
seoyuun <carpedm1221@gmail.com>
2022-01-20 23:36:10 +0900

    슬라이드 아이템 이미지 추가

--- src/components/list/ListItem.jsx
+++ src/components/list/ListItem.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./listItem.scss";
 
 const ListItem = () => {
-  return <div className="listItem">item</div>;
+  return (
+    <div className="listItem">
+      <img src="" alt="" />
+    </div>
+  );
 };
 
 export default ListItem;
