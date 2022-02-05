13adc6ac275ad0ae967d98959faafe56165a8ba2
seoyuun <carpedm1221@gmail.com>
2022-02-05 17:31:05 +0900

    listItem hover Info 추가

--- src/components/list/ListItem.jsx
+++ src/components/list/ListItem.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import "./listItem.scss";
+import React from 'react';
+import './listItem.scss';
 
 const ListItem = () => {
   return (
     <div className="listItem">
-      <img src="../src/image/editimage1.jpg" alt="" />
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
+      <>
+        <div className="itemInfo">
+          <div className="icons"></div>
+          <div className="itemInfoTop">
+            <span>1 hour 14 mins</span>
+            <span className="limit">+16</span>
+            <span>1999</span>
+          </div>
+          <div className="desc">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Praesentium hic rem eveniet error possimus, neque ex doloribus.
+          </div>
+          <div className="genre">Action</div>
+        </div>
+      </>
     </div>
   );
 };
